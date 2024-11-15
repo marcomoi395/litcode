345d66e03d75ab06f0dd7141b7abd3bf4965751e
youngmarco <marcomoi395@gmail.com>
2024-11-15 18:09:55 +0700

    update 3 more linked list problem

--- main.cpp
+++ main.cpp
@@ -2,24 +2,59 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *creatListFromArray(vector<int> nums) {
+    if (nums.empty()) return nullptr;
+
+    ListNode *head = new ListNode(nums[0]);
+    ListNode *current = head;
+    for (int i = 1; i < nums.size(); i++) {
+        current->next = new ListNode(nums[i]);
+        current = current->next;
+    }
+    return head;
+
+}
+
 class Solution {
 public:
-    static void backtracking(vector<vector<int>> &result, vector<int> &nums, vector<int> &cur, int i) {
-        if(i == nums.size() + 1) return;
-        result.push_back(cur);
+    static ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+        ListNode *head = new ListNode();
+        ListNode *cur = head;
 
-        for (int j = i; j < nums.size(); j++) {
-            cur.push_back(nums[j]);
-            backtracking(result, nums, cur, j + 1);
-            cur.pop_back();
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                cur->next = new ListNode(list1->val);
+                list1 = list1->next;
+            } else {
+                cur->next = new ListNode(list2->val);
+                list2 = list2->next;
+            }
+            cur = cur->next;
         }
-    }
 
-    static vector<vector<int>> subsets(vector<int> &nums) {
-        vector<vector<int>> result;
-        vector<int> cur;
-        backtracking(result, nums, cur, 0);
-        return result;
+        while (list1) {
+            cur->next = new ListNode(list1->val);
+            list1 = list1->next;
+            cur = cur->next;
+        }
+
+        while (list2) {
+            cur->next = new ListNode(list2->val);
+            list2 = list2->next;
+            cur = cur->next;
+        }
+        return head->next;
     }
 };
 
@@ -28,15 +63,18 @@ int main() {
     freopen("inputf.in", "r", stdin);
     freopen("outputf.out", "w", stdout);
 #endif
-    int n;
-    cin >> n;
-    vector<int> nums(n);
+    int n, m;
+    cin >> n >> m;
+    vector<int> nums(n), nums2(m);
     for (int &x: nums) cin >> x;
+    for (int &x: nums2) cin >> x;
 
-    vector<vector<int>> res = Solution::subsets(nums);
-
-    for (auto x: res) {
-        for (int y: x) cout << y << ' ';
-        cout << endl;
+    ListNode *l1 = creatListFromArray(nums);
+    ListNode *l2 = creatListFromArray(nums2);
+    ListNode *result = Solution::mergeTwoLists(l1, l2);
+    ListNode *current = result;
+    while (current) {
+        cout << current->val << ' ';
+        current = current->next;
     }
 }
